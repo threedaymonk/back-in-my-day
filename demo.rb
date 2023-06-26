c133d1044578fa7b84e6e1dd2c13461abd2ab952
Paul Battley <pbattley@gmail.com>
2023-06-26 14:59:52 +0100

    Add benchmarking via "benchmark" option

--- demo.rb
+++ demo.rb
@@ -3,10 +3,25 @@ $LOAD_PATH.unshift File.join(__dir__, "lib")
 require "datastore/collection"
 require "movie"
 
+def filter(movies)
+  movies
+    .select { |m| m in { title: /godzilla|gojira/i, score: 6.5.. } }
+    .sort_by(&:year)
+end
+
 movies = Datastore::Collection.new("kaiju.json", Movie)
 
-filtered = movies
-  .select { |m| m in { title: /godzilla|gojira/i, score: 6.5.. } }
-  .sort_by(&:year)
+if ARGV.first == "benchmark"
+  require "benchmark"
 
-puts filtered.join("\n")
+  Benchmark.bm do |bm|
+    bm.report do
+      10_000.times do
+        filtered = filter(movies)
+      end
+    end
+  end
+else
+  filtered = filter(movies)
+  puts filtered.join("\n")
+end
