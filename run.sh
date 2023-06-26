2961e910ae38027e3bea87584d951af3637ccff7
Paul Battley <pbattley@gmail.com>
2023-06-26 17:30:52 +0100

    Run in Ruby 1.6.8

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-docker run -v $(pwd):/root/back-in-my-day -it ruby_1_8_7 bash -c "\
-  source ~/.bashrc && \
+docker run --platform i386 -v $(pwd):/root/back-in-my-day -it ruby_1_6_8 bash -c "\
   cd ~/back-in-my-day && \
-  ruby -rubygems demo.rb $1" | fromdos
+  ~/ruby/ruby -I ~/ruby/lib demo.rb $1" | fromdos
