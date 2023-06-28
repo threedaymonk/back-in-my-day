5ed297f44c73d29245d75e5a51f87747f579cf0b
Paul Battley <pbattley@gmail.com>
2023-06-28 13:17:44 +0100

    Fix: <<~ heredoc doesn't exist
        syntax error, unexpected << (SyntaxError)

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,12 +1,12 @@
 Movie = Struct.new(:title, :year, :score, :directors, :actors, keyword_init: true) do
   def to_s
-    <<~STRING
-      #{title}
-        Year:     #{year}
-        Score:    #{stars} #{format("%0.1f", score)}
-        Director: #{directors.join(", ")}
-        Actors:   #{actors.join(", ")}
-    STRING
+    <<STRING
+#{title}
+  Year:     #{year}
+  Score:    #{stars} #{format("%0.1f", score)}
+  Director: #{directors.join(", ")}
+  Actors:   #{actors.join(", ")}
+STRING
   end
 
   private def stars
