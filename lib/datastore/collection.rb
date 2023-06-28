0e325264e6b6a2af2f2805c3f18f4b6aaa33fc94
Paul Battley <pbattley@gmail.com>
2023-06-28 13:54:04 +0100

    Fix: can't use fully qualified name in class definition
        syntax error (SyntaxError)
        class Datastore::Collection
                         ^

--- lib/datastore/collection.rb
+++ lib/datastore/collection.rb
@@ -1,15 +1,17 @@
 require "datastore"
 require "json"
 
-class Datastore::Collection
-  include Enumerable
+module Datastore
+  class Collection
+    include Enumerable
 
-  def initialize(path, item_class)
-    @items = JSON.load(File.read(path))
-    @item_class = item_class
-  end
+    def initialize(path, item_class)
+      @items = JSON.load(File.read(path))
+      @item_class = item_class
+    end
 
-  def each
-    @items.each { |h| yield @item_class.new(h) }
+    def each
+      @items.each { |h| yield @item_class.new(h) }
+    end
   end
 end
